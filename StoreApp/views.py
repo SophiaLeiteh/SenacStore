b217b9428707cd32939f57977011694c79b0a31d
SOPHIA <sophialeite@gmail.com>
2024-04-10 19:07:51 -0300

    Alteração na Estrutura

--- StoreApp/views.py
+++ StoreApp/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
-
+from StoreApp.models import Departamento
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    departamentos = Departamento.objects.all()
+
+    context = {
+        'departamentos' : departamentos
+    }
+    return render(request, 'index.html', context)
+
 
 def produtos_lista(request):
     return render(request, 'produtos.html')
