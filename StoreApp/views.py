b26f7d8fc630ecedeeebf21a862afb1fc2fab7bb
SOPHIA <sophialeite@gmail.com>
2024-04-15 22:20:09 -0300

    Criação de página de contato e cadastro

--- StoreApp/views.py
+++ StoreApp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from StoreApp.models import Departamento, Produto
+from StoreApp.forms import CadastroForm, ContatoForm
 # Create your views here.
 def index(request):
     produtos_destaque = Produto.objects.filter(destaque = True)
@@ -39,3 +40,52 @@ def produtos_lista_por_departamento(request, id):
     }
 
     return render(request, 'produtos.html', context)
+
+def produto_detalhe(request, id):
+    produto = Produto.objects.get(id = id)
+    produtos_relacionados = Produto.objects.filter(departamento_id = produto.departamento).exclude(id = id)[:4]
+
+    context = {
+        'produto' : produto,
+        'produtos_relacionados' : produtos_relacionados
+    }
+    return render(request, 'produto_detalhes.html', context)
+
+def sobre_empresa(request):
+    return render(request, 'sobre_empresa.html')
+
+def cadastro(request):
+    #armazenar msg de sucesso ou erro
+    mensagem = ''
+    #se o formulario foi submetido
+    if request.method == 'POST':
+        formulario = CadastroForm(request.POST)
+        if formulario.is_valid():
+            formulario.save()
+            formulario = CadastroForm()
+            mensagem = "Cliente cadastrado com sucesso."
+        else:
+            mensagem = "Verifique os erros abaixo: "
+
+    #se o formulario não foi submetido. Entrei na pag. pelo menu
+    #e o form deve vir vazio
+    else:
+        formulario = CadastroForm()
+
+    context = {
+        'formulario_cadastro' : formulario,
+        'mensagem' : mensagem 
+    }
+    return render(request, 'cadastro.html', context)
+
+def contato(request):
+    mensagem = ''
+    formulario = ContatoForm()
+
+
+    context = {
+        'mensagem': mensagem,
+        'formulario_contato' : formulario
+
+    }
+    return render(request, 'contato.html', context)
